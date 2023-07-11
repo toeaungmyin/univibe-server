949e5f06bd9c3d8ca507dc30fa85a8f342302abc
toeaungmyin-noobie <toeaungmyin.official@gmail.com>
2023-07-11 08:34:12 +0630

    cors fix 2

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,8 +17,9 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header("Access-Control-Allow-Origin", "http://localhost:3000") // Already tried with *
-            ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-            ->header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Headers', ' Origin, Content-Type, Accept, Authorization, X-Request-With')
+            ->header('Access-Control-Allow-Credentials', ' true');
     }
 }
