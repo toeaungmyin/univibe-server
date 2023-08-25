d3a0417a908277d19b00c367ec8c79ad6121be83
toeaungmyin-noobie <toeaungmyin.official@gmail.com>
2023-08-25 13:13:57 +0630

    Passport key generate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        Passport::loadKeysFrom(__DIR__ . '/../secrets/oauth');
 
         Passport::tokensExpireIn(now()->addDays(15));
         Passport::refreshTokensExpireIn(now()->addDays(30));
