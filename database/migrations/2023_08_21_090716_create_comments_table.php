7d7346e13a1653f6ce83a85c2c7b6fd0e6f50689
toeaungmyin-noobie <toeaungmyin.official@gmail.com>
2023-08-30 14:20:35 +0630

    Comment DB Fix

--- database/migrations/2023_08_21_090716_create_comments_table.php
+++ database/migrations/2023_08_21_090716_create_comments_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->text('comment');
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
         });
     }
